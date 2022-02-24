a2d4f0375b37c2dc6c77fe9f1f0c4c539dd3f278
jahanzaib04 <jahanzaibalikhan04@gmail.com>
2022-02-24 12:35:09 +0500

    Complete Project

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+app.options('*', cors());
+
+// Route Imports
+const user = require('./routes/userRoute');
+app.use('/api/v1', user);
+
+app.use(express.static(path.join(__dirname, "./game-task/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./game-task/build/index.html"));
+  });
+
+module.exports = app;
